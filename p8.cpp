a9888edc8fc6d2f5a742bc1e53c3d0d0710e9337
mkilian <mkilian@mielec.com.pl>
2017-11-16 15:05:58 +0100

    osiem cpp

---
+++ p8.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int Szukaj(int T[], int n, int k, int p)
+{
+  int i;
+
+  T[n] = k;
+  for(i = p; T[i] != k; i++) ;
+  if(i == n) return -1; else return i;
+}
+
+int main()
+{
+  int * Z,n,k,i;
+  
+  cin >> n;
+  Z = new int [n + 1];
+  for(i = 0; i < n; i++) cin >> Z[i];
+  cin >> k;
+  cout << endl << Szukaj(Z,n,k,0) << endl << endl;
+  delete [] Z;
+  return 0;
+} 
