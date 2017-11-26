d09ae32b511877af30ed277170f18925fb0a6c9d
Kobas11 <wiktor431@op.pl>
2017-11-26 10:06:00 +0100

    Zmiana w p2.cpp

--- p2.cpp
+++ p2.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 const double eps = 1e-12;
 
-//---------------------------------------
+//--------------------------------------
 bool ludist(int n, double ** A)
 {
   int i,j,k;
